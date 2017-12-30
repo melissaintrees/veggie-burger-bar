1a91351cb12f35fbd1b96214239ace911de37806
melissaintrees <melissaintrees@gmail.com>
2017-12-29 22:43:05 -0700

    all

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "cr8lifenow222",
-  database: "veggieburgers_db"
-});
+var connection;
+
+// Make connection.
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   connection= mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "cr8lifenow222",
+      database: "veggieburgers_db"
+  });
+};
+
+
 console.log("connected to mysql on port: " + connection.config.port);
 // Make connection.
 connection.connect(function(err) {
