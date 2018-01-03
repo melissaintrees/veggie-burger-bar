baba33ec5dfdf8c57aa09ed61f5f1014d11b70ea
melissaintrees <melissaintrees@gmail.com>
2018-01-02 22:01:30 -0600

    make change

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ USE  c7nvz2ld5h6nv2a4;
 
 CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
-  burger_name varchar(300) NOT NULL,
+  veggieburger_name varchar(300) NOT NULL,
   devoured BOOLEAN DEFAULT false,
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   date_consumed TIMESTAMP,
